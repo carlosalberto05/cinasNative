e30e40a300d2cfdaf835066aebbb78d9b9b224c7
carlosalberto05 <carlos_alberto-lira@outlook.com>
2020-04-27 10:08:42 -0500

    Agregando flatlist

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
 
 const App = () => {
+  const [citas, setCitas] = useState([
+    {id: '1', paciente: 'Mozart', propietario: 'Carlos', sintomas: 'No come'},
+    {id: '2', paciente: 'Sanson', propietario: 'Renata', sintomas: 'No duerme'},
+    {id: '3', paciente: 'Lucas', propietario: 'Erika', sintomas: 'No canta'},
+  ]);
+
   return (
     <>
       <View style={styles.contenedor}>
         <Text style={styles.titulo}>Administrador de Citas </Text>
+
+        <FlatList
+          data={citas}
+          renderItem={({item}) => {
+            return (
+              <View>
+                <Text>{item.paciente}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(cita) => cita.id}
+        />
+
+        {/* {citas.map((cita) => {
+          return (
+            <View>
+              <Text>{cita.paciente}</Text>
+            </View>
+          );
+        })} */}
       </View>
     </>
   );
